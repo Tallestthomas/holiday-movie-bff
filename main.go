8f94870fe5d97fe4c82c618c3bfb4cdf993c4049
Thomas <trasmussen@redventures.com>
2019-03-24 20:29:11 -0400

    adding db service

--- main.go
+++ main.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-	"github.com/globalsign/mgo"
 	"github.com/go-chi/chi"
+	"holiday-movie-bff/models"
 	"net/http"
-	"time"
 )
-
-const (
-	host       = "db:27017"
-	database   = "db"
-	username   = ""
-	password   = ""
-	collection = "squares"
-)
-
-func initMongo() (session *mgo.Session) {
-	info := &mgo.DialInfo{
-		Addrs:    []string{host},
-		Timeout:  60 * time.Second,
-		Database: database,
-		Username: username,
-		Password: password,
-	}
-
-	session, err := mgo.DialWithInfo(info)
-	if err != nil {
-		panic(err)
-	}
-	return session
-}
 
 func main() {
 	r := chi.NewRouter()
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+	initMongo()
 	http.ListenAndServe(":3000", r)
 }
