e7271034e3c7ad69f13a2ef48e40f3db77ea8a40
Thomas <trasmussen@redventures.com>
2019-03-26 09:06:49 -0400

    Mongodb connection is working, need to structure handlers better

--- main.go
+++ main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"github.com/globalsign/mgo"
 	"github.com/go-chi/chi"
+	"holiday-movie-bff/models"
 	"holiday-movie-bff/models/mongo"
 	"net/http"
 )
 
 // App struct
 type App struct {
-	session *mongo.Session
+	session *mgo.Session
 }
 
 func main() {
 	r := chi.NewRouter()
 
-	mg, err := mongo.NewSession()
+	var app App
+	var err error
+	app.session, err = mongo.NewSession()
 	if err != nil {
 		panic(err)
 	}
 
-	defer mg.Close()
+	r.HandleFunc("/squares", models.GetSquares(session))
+	defer session.Close()
 
 	http.ListenAndServe(":3000", r)
 }
--- models/squares.go
+++ models/squares.go
@@ -1,7 +1,30 @@
 package models
 
+import (
+	"encoding/json"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"net/http"
+)
+
 // Square model
 type Square struct {
-	ID      string `json:"id" bson:"id"`
+	ID      string `json:"_id" bson:"_id"`
 	Content string `json:"content" bson:"content"`
+}
+
+// GetSquares returns all squares
+func GetSquares(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	session := s.Copy()
+
+	c := session.DB("bingodb").C("squares")
+	var squares []Square
+	err := c.Find(bson.M{}).All(&squares)
+	if err != nil {
+		panic(err)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(squares)
+	}
 }
